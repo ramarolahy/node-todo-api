f56d562159f4ce6d1848dcbbe23e11e73dc45a78
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-02-01 14:31:02 -0700

    modified gitignore

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Vendor imports;
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //Local
 const {mongoose} = require('./db/mongoose');
@@ -25,10 +26,30 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then( todos => {
-        res.send({todos})
+        res.send({todos}); // Using todo as object will give us flexibility to add other fields/params later
     }, err => {
         res.status(400).send(err);
     });
+});
+
+// Getting an individual resource
+app.get('/todos/:id', (req, res) => {
+    // testing :id param with Postman
+    //res.send(req.params);
+
+    const id = req.params.id;
+    // Validate id using isValid()
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send() // Insert awesome error message handling inside send
+    }
+
+    Todo.findById(id).then(todo => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo}) // Using todo as object will give us flexibility to add other fields/params later
+    }).catch( err => res.status(400).send()); // Insert awesome error message handling inside send
+
 });
 
 app.listen(3000, () => {
