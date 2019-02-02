0eb243305c5c11f739a8e33c034c2cfe968b0699
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-02-02 13:41:23 -0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 // Vendor imports;
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const {
+    ObjectID
+} = require('mongodb');
 
 //Local
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const {
+    mongoose
+} = require('./db/mongoose');
+const {
+    Todo
+} = require('./models/todo');
+const {
+    User
+} = require('./models/user');
 
 const app = express();
 // Set up port to allow heroku to set up env
@@ -21,7 +30,7 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then( doc => {
+    todo.save().then(doc => {
         res.send(doc);
     }, err => {
         res.status(400).send(err);
@@ -29,8 +38,10 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then( todos => {
-        res.send({todos}); // Using todo as object will give us flexibility to add other fields/params later
+    Todo.find().then(todos => {
+        res.send({
+            todos
+        }); // Using todo as object will give us flexibility to add other fields/params later
     }, err => {
         res.status(400).send(err);
     });
@@ -43,16 +54,18 @@ app.get('/todos/:id', (req, res) => {
 
     const id = req.params.id;
     // Validate id using isValid()
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         res.status(404).send() // Insert awesome error message handling inside send
     }
 
     Todo.findById(id).then(todo => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send();
         }
-        res.send({todo}) // Using todo as object will give us flexibility to add other fields/params later
-    }).catch( err => res.status(400).send()); // Insert awesome error message handling inside send
+        res.send({
+            todo
+        }) // Using todo as object will give us flexibility to add other fields/params later
+    }).catch(err => res.status(400).send()); // Insert awesome error message handling inside send
 
 });
 
@@ -60,21 +73,62 @@ app.delete('/todos/:id', (req, res) => {
     // get the id
     const id = req.params.id;
     //validate id
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         res.status(404).send()
     }
 
     Todo.findByIdAndDelete(id).then(todo => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send()
         }
-        res.send({todo})
-    }).catch (err => res.status(400).send());
+        res.send({
+            todo
+        })
+    }).catch(err => res.status(400).send());
 })
+
+app.patch('/todos/:id', (req, res) => {
+    // get id
+    const id = req.params.id;
+    // get body
+    // _.pick will allow us to restrict the user from modifying/adding unwanted properties
+    // We don't want the user to update id or completedAt.
+    // see https://lodash.com/docs/4.17.11#pick 
+    const body = _.pick(req.body, ['text', 'completed'])
+
+    //validate id
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send()
+    }
+    // If user updates complete and is a boolean
+    //
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    // Update the db by using mongodb operators. Mongoose use new: instead of returnOriginal
+    // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate 
+    Todo.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then( todo => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch( err => {
+        res.status(400).send();
+    })
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
 })
 
 
-module.exports = {app};
+module.exports = {
+    app
+};
